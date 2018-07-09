89aff3195ba1c3393881854bba0f27eb2f6db63e
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-07-09 16:39:47 -0700

    Create test-sqlconnection.ps1

---
+++ test-sqlconnection.ps1
@@ -0,0 +1,25 @@
+function Test-SQLConnection
+{    
+    [OutputType([bool])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $ConnectionString
+    )
+    try
+    {
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $ConnectionString;
+        $sqlConnection.Open();
+        $sqlConnection.Close();
+
+        return $true;
+    }
+    catch
+    {
+        return $false;
+    }
+}
+
+Test-SQLConnection "Server=(local)\SQL2017;Database=master;User ID=sa;Password=Password12!"
